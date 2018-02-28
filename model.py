7fb3a5b69a3ff9eacbb23a8cd1d806044edf6495
Jordan <jordan.bouchoucha@ntymail.com>
2018-02-28 22:33:12 +0100

    simple model

---
+++ model.py
@@ -0,0 +1,9 @@
+"""
+Fonction qui permet de parser
+l'entrée et renvoie la data
+"""
+def get_input():
+  data = {}
+
+  return data
+  
---
+++ solver.py
@@ -0,0 +1,8 @@
+"""
+Fonction qui prend en entrée
+la data parser en entrée et
+résout le problème
+"""
+def solver_v1(data):
+
+  return
