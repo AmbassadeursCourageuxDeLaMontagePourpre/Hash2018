8831cc6c962b7d7c00a86b395b590687fb5c7895
Jordan <jordan.bouchoucha@ntymail.com>
2018-03-01 21:21:28 +0100

    new solver heuristic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import solver
 import sys 
 
 files = ["a_example.in", "b_should_be_easy.in", "c_no_hurry.in", "d_metropolis.in", "e_high_bonus.in"]
+#files = ["a_example.in"]
 for f in files:
-  solver.solver_greedy_time('examples/'+f)
+  solver.solver_heuristic('examples/'+f)
 
--- solver.py
+++ solver.py
@@ -1,5 +1,6 @@
 from model import * 
 from helping import *
+import numpy as np
 
 """
 Fonction qui résout le problème
@@ -29,12 +30,24 @@ cherchant toujours le plus proche
 def solver_heuristic(filename):
   data = get_input(filename)
   vehicules = data['n_vehicles']
+  rides = data['rides']
+  new_rides = sorted(rides, key=lambda x: x.earliest_start)
   distributions = [[] for _ in range(vehicules)]
 
-  new_rides = sorted(rides, key=lambda x: x.earliest_start)
+  vehicules_states = [[(0, 0), 0] for _ in range(vehicules)]
 
-  # Tant qu'il y a encore des rides
+  # Pour chaque ride
   for ride in new_rides:
-    
+    # On calcule pour cette ride, quelle voiture
+    # donne le meilleur score en la prenant maintenant
+    value_vehicules = [-1 for _ in range(vehicules)]
+    for veh in range(vehicules):
+      value_vehicules[veh] = ride_score(vehicules_states[veh][0][0], vehicules_states[veh][0][1], vehicules_states[veh][1], ride, data)[1]
+    chosen_vehicule = np.argmax(value_vehicules) 
+    distributions[chosen_vehicule].append(ride.id)
+
+    # On met à jour l'état du véhicule
+    vehicules_states[chosen_vehicule][0] = (ride.end_x, ride.end_y)
+    vehicules_states[chosen_vehicule][1] = max(ride.earliest_start, vehicules_states[chosen_vehicule][1]) + ride.distance 
 
   write_distributions(distributions, filename)
