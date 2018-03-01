40f436d2c025e0fd59a0c7f547e0152c4da17add
Jordan <jordan.bouchoucha@ntymail.com>
2018-03-01 19:18:59 +0100

    solver greedy

---
+++ main.py
@@ -0,0 +1,5 @@
+import solver
+import sys 
+
+filename = sys.argv[1]
+solver.solver_greedy_time(filename)
--- solver.py
+++ solver.py
@@ -1,9 +1,28 @@
-from model import get_input
+from model import * 
 
 """
 Fonction qui résout le problème
+de façon greedy selon le temps
 """
-def solver_v1():
-  data = get_input()
+def solver_greedy_time(filename):
+  data = get_input(filename)
+  rides = data['rides']
+  vehicules = data['n_vehicules']
+  distributions = [[] for i in range(vehicules)]
 
-  return
+  new_rides = sorted(rides, key=lambda x: x.earliest_start)
+
+  cpt = 0
+  for ride in new_rides:
+    if cpt == vehicules:
+      cpt = 0
+    distributions[cpt].append(ride.id)
+    cpt+=1
+
+  f = open("output.out", 'w')
+  for dis in distributions:
+    line = str(len(dis)) 
+    for d in dis:
+      line += d + ' '
+    f.write(line)
+  f.close()
